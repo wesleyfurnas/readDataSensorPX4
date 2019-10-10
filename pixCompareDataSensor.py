dc277c2cf455e0ad0b03484d19848bb1558c6ac0
wesleyfurnas <wesleyfurnas35@gmail.com>
2019-10-10 10:00:04 -0300

    graph compare data in time

--- pixCompareDataSensor.py
+++ pixCompareDataSensor.py
@@ -4,28 +4,25 @@ import math
 ## script comparacao dos dados do mesmo sensor em leituras diferentes 
 
 ##         localizacao do arquivo com os dados do arquivo1
-f2 = open('17_21_02_vehicle_air_data_0.csv ', 'r' )
+f2 = open('/home/wesley/Desktop/pixCSVTESTE1/pixCSVTESTE2/17_21_02_vehicle_air_data_0.csv', 'r' )
 
 
 ##         localizacao do arquivo com os dados do arquivo2
-f = open('17_21_02_sensor_combined_0.csv', 'r' )
-f.close()
+f = open('/home/wesley/Desktop/pixCSVTESTE1/pixCSVTESTE2/17_21_02_sensor_combined_0.csv', 'r' )
+
 ##	vetores da leitura 1 
 temp1  = []
-roll1  = []
-pitch1 = []
-yaw1   = []
+accelZ = []
 ## 	vetores da leitura 2 
+
+fim = 0
 
 altura = []
 	
 leitor2 = csv.reader(f2)
 leitor = csv.reader(f)
-#leitor2 = csv.reader(f2)
 cont  = 0 ## contadores pra saltar linhas com texto
 
-
-cont = 0
 
 for linha in leitor2:
    if  cont == 0 :
@@ -36,12 +33,28 @@ for linha in leitor2:
        cont = cont +1
 
 
-plt.subplot(2, 1, 1)
-plt.plot(temp1,roll1)
+fim = cont 
+cont = 0
+
+for linha in leitor:
+   if  cont == 0 :
+       cont = cont + 1
+   else :
+        if fim > cont: 
+           accelZ.append(float(linha[8]))
+           cont = cont +1
+        else :
+           cont = cont + 1
+
+f2.close()
+f.close()
+
+plt.subplot(3, 2, 1)
 plt.plot(temp1,altura)
+plt.plot(temp1,accelZ)
 
 plt.title('altura x tempo ')
-plt.ylabel('altura')
+plt.ylabel('altura/aceleracao')
 plt.xlabel('time ')
 
 plt.show()
